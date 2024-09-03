81c2745b2396829fe18fe53cfd3688290b746695
fikrymusa <81131306+fikrymusa@users.noreply.github.com>
2024-09-03 12:55:41 +0700

    initial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  keyframes: {
+			slideInRight: {
+			  '0%': { transform: 'translateX(100%)' },
+			  '100%': { transform: 'translateX(0)' },
+			},
+			slideOutRight: {
+			  '0%': { transform: 'translateX(0)' },
+			  '100%': { transform: 'translateX(100%)' },
+			},
+		  },
+		  animation: {
+			slideInRight: 'slideInRight 0.5s ease-out',
+			slideOutRight: 'slideOutRight 0.5s ease-in',
+		  },
+		},
+	  },
 	plugins: [],
 }
