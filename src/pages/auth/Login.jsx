e2163b3c2b4dc8f2b42f2dbec9ccf5ef986e72eb
fikrymusa <81131306+fikrymusa@users.noreply.github.com>
2024-09-05 11:55:20 +0700

    initial

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -28,11 +28,11 @@ const Login = () => {
         });
     }
   return (
-    <div className="flex items-center min-h-screen bg-white dark:bg-gray-900">
+    <div className="flex items-center min-h-screen bg-gray-900">
             <div className="container mx-auto">
                 <div className="max-w-md mx-auto my-10">
                     <div className="text-center">
-                        <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Sign in To AIRIS</h1>
+                        <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Sign in To <span className="text-cyan-400">AIRIS</span></h1>
                         <p className="text-gray-500 dark:text-gray-400">Sign in to access your account</p>
                     </div>
                     <div className="m-7">
