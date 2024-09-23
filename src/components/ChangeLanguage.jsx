4cc744deede4a0853d1f60606e16437a5fa9052c
fikrymusa <81131306+fikrymusa@users.noreply.github.com>
2024-09-23 16:24:20 +0700

    initial

--- src/components/ChangeLanguage.jsx
+++ src/components/ChangeLanguage.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ChangeLanguage = () => {
     const handleChange = (e) => {
-        // alert(e.target.value)
         localStorage.setItem("lang", e.target.value);
     }
+
+
     const lang = localStorage.getItem("lang");
-    // console.log("lang", lang)
+
   return (
     <select className='text-xs'
     onChange={(e)=>handleChange(e)}
